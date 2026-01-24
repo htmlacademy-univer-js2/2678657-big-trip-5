1d07b7aa979ed500a08e1c8841daf294276af6a2
Siestalove <willovv4u@gmail.com>
2026-01-24 20:54:06 +0700

    Исправил ошибку и перенес отрисовку из мэйна в пресентер

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -1,12 +1,17 @@
-import { render } from '../render';
+import { render } from '../render.js';
 import CreateFormView from '../view/create-form.js';
 import EditFormView from '../view/edit-form.js';
 import RoutePointView from '../view/route-point.js';
 import ListRoutePointView from '../view/list-route-point.js';
+import FilterView from '../view/filters.js';
+import SortingView from '../view/sorting.js';
+import TripInfoView from '../view/trip-info.js';
 
 export default class Presenter {
-  constructor({container}) {
+  constructor({container, filtersContainer, tripMainContainer}) {
     this.container = container;
+    this.filtersContainer = filtersContainer;
+    this.tripMainContainer = tripMainContainer;
   }
 
   init(){
@@ -14,11 +19,14 @@ export default class Presenter {
     const editFormView = new EditFormView();
     const listRoutePointView = new ListRoutePointView();
 
+    render(new TripInfoView(), this.tripMainContainer, 'afterbegin');
+    render(new FilterView(), this.filtersContainer);
+    render(new SortingView(), this.container);
     render(editFormView, listRoutePointView.getElement());
     render(createFormView, listRoutePointView.getElement());
-    render(listRoutePointView, this.container);
     for(let i = 0; i < 3; i++){
-      render(new RoutePointView(), this.listRoutePointView.getElement());
+      render(new RoutePointView(), listRoutePointView.getElement());
     }
+    render(listRoutePointView, this.container);
   }
 }
