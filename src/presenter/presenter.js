f31520a8dc7a7905b0c65b38f1bb0a9ba69b7c19
Siestalove <willovv4u@gmail.com>
2026-02-10 01:11:09 +0700

    Fixed sorting attributes and constructor

--- src/presenter/presenter.js
+++ src/presenter/presenter.js
@@ -109,6 +109,7 @@ export default class Presenter {
 
   #renderSort() {
     this.#sortComponent = new SortingView({
+      currentSortType: this.#currentSortType,
       onSortTypeChange: this.#handleSortTypeChange
     });
 
